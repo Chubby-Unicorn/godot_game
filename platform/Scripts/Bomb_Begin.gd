8915913705bcb0a8e3fd544377b3b5a9c36371c5
Lezorical <juan.arreola121599@gmail.com>
2025-02-12 13:26:16 -0800

    added zoom

--- platform/Scripts/Bomb_Begin.gd
+++ platform/Scripts/Bomb_Begin.gd
@@ -5,12 +5,12 @@ var times_to_play : int = 1
 @export_group("Bomb Timer in seconds")
 @export var bomb_timer : float #in seconds
 
-func begin_animation():
+func _begin_animation():
 	$Animation.play() #star animation
 	$"../Sound Manager/BombTickSound".play()
-	explode_bomb()
+	_explode_bomb()
 	
-func explode_bomb():
+func _explode_bomb():
 	
 	await get_tree().create_timer(bomb_timer).timeout #Countdown
 	$"../Sound Manager/BombTickSound".stop()
@@ -18,8 +18,26 @@ func explode_bomb():
 	
 	queue_free()
 
-func _input(event):
-	if event is InputEventMouseButton:
-		if times_to_play == 1:
-			times_to_play -= 1
-			begin_animation()
+
+func _ready() -> void:
+	_begin_animation()
+	
+	
+#This one or
+#func _input_event(viewport, event, shape_idx):
+#	if event is InputEventMouseButton:
+#		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+#			print("You clicked on Sprite!")
+
+#or This one
+#func _input(event):
+#	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
+#		if get_viewport_rect().has_point(to_local(event.position)):
+#			pass
+
+#func _input_event(viewport, event, shape_idx):
+#	if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+#		if times_to_play == 1:
+#			times_to_play -= 1
+#			print("bombBegin")
+#			_begin_animation()
---
+++ platform/Scripts/zoom.gd
@@ -0,0 +1,24 @@
+extends Camera2D
+
+@export_group("Max Zoom")
+
+@export var max_zoom_in : float
+@export var max_zoom_out : float
+
+func _input(event):
+	if event is InputEventMouseButton and event.pressed:
+		if event.button_index == MOUSE_BUTTON_WHEEL_UP :
+			$".".zoom.x = 2
+			$".".zoom.y = 2
+		if event.button_index == MOUSE_BUTTON_WHEEL_DOWN:
+			$".".zoom.x = 0.5
+			$".".zoom.y = 0.5
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
