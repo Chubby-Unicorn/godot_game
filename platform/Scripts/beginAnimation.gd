b1f47f54721c39ab6129c6cfd570905d6b7c9e32
Lezorical <juan.arreola121599@gmail.com>
2025-02-11 20:20:49 -0800

    click to make bomb tick

--- platform/Scripts/beginAnimation.gd
+++ platform/Scripts/beginAnimation.gd
@@ -1,9 +1,13 @@
 extends AnimatedSprite2D
 
+var times_to_play : int = 1
+
 func begin_animation():
 	$".".play()
+	$"../../Sound Manager".bomb_sound()
 
 func _input(event):
 	if event is InputEventMouseButton:
-		begin_animation()
-		
+		if times_to_play == 1:
+			times_to_play -= 1
+			begin_animation()
